848b48aeb1f722a234546a34374f38cdbfe6a02c
Ben Greene <BenJGreene@gmail.com>
2026-08-11 22:34:39 +0100

    Add router url

--- aad-fe-container/src/app/app.routes.ts
+++ aad-fe-container/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: 'agent-registry', pathMatch: 'full' },
   { path: 'agent-registry', loadComponent: () => import('./components/agent-registry/agent-registry.component').then(m => m.AgentRegistryComponent) },
+  { path: 'agent-registry/:id', loadComponent: () => import('./components/agent-registry/agent-registry.component').then(m => m.AgentRegistryComponent) },
+
   { path: 'interactive-testing', loadComponent: () => import('./components/interactive-testing/interactive-testing.component').then(m => m.InteractiveTestingComponent) },
   { path: 'network-visualizer', loadComponent: () => import('./components/network-visualizer/network-visualizer.component').then(m => m.NetworkVisualizerComponent) },
   { path: 'refactoring-lab', loadComponent: () => import('./components/refactoring-lab/refactoring-lab.component').then(m => m.RefactoringLabComponent) },
--- aad-fe-container/src/app/components/agent-registry/agent-registry.component.spec.ts
+++ aad-fe-container/src/app/components/agent-registry/agent-registry.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 import { AgentRegistryComponent } from './agent-registry.component';
 
 describe('AgentRegistryComponent', () => {
@@ -11,9 +12,10 @@ describe('AgentRegistryComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AgentRegistryComponent],
-      providers: [provideHttpClient(), provideAnimationsAsync()]
+      providers: [provideHttpClient(), provideAnimationsAsync(), provideRouter([])]
     })
     .compileComponents();
+
 
     fixture = TestBed.createComponent(AgentRegistryComponent);
     component = fixture.componentInstance;
