42b3f06f0fe79f6be9e2e61ffd3409ba50d2439f
Ben Greene <BenJGreene@gmail.com>
2026-08-23 11:30:52 +0100

    fix: update test spec mocks and module imports to use tool-manager

--- aad-fe-container/src/app/components/tool-manager/tool-manager.component.spec.ts
+++ aad-fe-container/src/app/components/tool-manager/tool-manager.component.spec.ts
@@ -2,24 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { McpManagerComponent } from './mcp-manager.component';
+import { ToolManagerComponent } from './tool-manager.component';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('McpManagerComponent', () => {
-  let component: McpManagerComponent;
-  let fixture: ComponentFixture<McpManagerComponent>;
+describe('ToolManagerComponent', () => {
+  let component: ToolManagerComponent;
+  let fixture: ComponentFixture<ToolManagerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [McpManagerComponent],
+      imports: [ToolManagerComponent],
       providers: [provideHttpClient(), provideAnimationsAsync(), { provide: ActivatedRoute, useValue: { paramMap: of({ get: () => null }), queryParams: of({}), snapshot: { paramMap: { get: () => null } } } }],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(McpManagerComponent);
+    fixture = TestBed.createComponent(ToolManagerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
