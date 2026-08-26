9732eeab30ffdea0b9840f4ded0c47e191d425b8
Ben Greene <BenJGreene@gmail.com>
2026-08-26 21:01:01 +0100

    Update configs and error propagation for llm

--- aad-be-container/src/lib.rs
+++ aad-be-container/src/lib.rs
@@ -11,10 +11,27 @@ pub mod traits;
 pub mod tokio_tools;
 pub mod threads;
 
+#[derive(Clone, Debug)]
+pub struct AppState {
+    pub pool: sqlx::PgPool,
+    pub config: config::AppConfig,
+}
 
+impl axum::extract::FromRef<AppState> for sqlx::PgPool {
+    fn from_ref(state: &AppState) -> Self {
+        state.pool.clone()
+    }
+}
+
+impl axum::extract::FromRef<AppState> for config::AppConfig {
+    fn from_ref(state: &AppState) -> Self {
+        state.config.clone()
+    }
+}
 
 pub const NAME: &str = "aad-be";
 pub const VERSION: &str = "0.1.0";
+
 
 
 
