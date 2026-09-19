d4b88c50b20a71b26307c7df5b780a799c75e274
Ben Greene <BenJGreene@gmail.com>
2026-09-19 07:05:42 +0100

    Add promethues export of tokio fixes

--- aad-be-container/src/lib.rs
+++ aad-be-container/src/lib.rs
@@ -100,8 +100,9 @@ pub async fn service_main(
 
     // HaMS Prometheus Registration
     let handle_clone = Arc::clone(&app_state.prometheus_handle);
+    let tokio_handle = app_state.tokio_handle.clone();
     hams_harness.hams.register_prometheus_closure(move || {
-        handle_clone.render()
+        crate::metrics::render_prometheus_metrics(&handle_clone, &tokio_handle)
     }).map_err(|e| format!("Failed to register Prometheus closure with HaMS: {e}"))?;
 
     // 5. Start Axum Main REST Webservice
